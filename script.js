bcb52e1a0917d5f85ff2302a3abf49cfdc67a9bc
Yixin <yg2841@nyu.edu>
2023-05-11 20:52:11 -0400

    Uploading Works.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+AOS.init();
+
+var swiper = new Swiper(".swpier", {
+  direction: "vertical",
+  pagination: {
+    el: '.swiper-pagination',
+      },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
